08b0c5e129a401344bd9f2accd8f91686b5bb030
maheleet <leetmahe@gmail.com>
2023-08-22 12:59:30 +0530

    update bash

--- buil1.sh
+++ buil1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Get the current image tag from the Docker Compose file
-current_tag=$(grep -oP 'image: ubuntu:\K\d+' apache.yml)
+current_tag=$(grep -oP 'image: ubuntu:\K\d+' apache.yaml)
 
 # Increment the tag by 1
 new_tag=$((current_tag + 1))
 
 # Build the Docker image with the new tag
 if docker build -t ubuntu:${new_tag} .; then
-    sed -i "s|image: ubuntu:\d\+|image: ubuntu:${new_tag}|" docker-compose.yml
+    sed -i "s|image: ubuntu:\d\+|image: ubuntu:${new_tag}|" apache.yaml
 
     echo "Docker image built and image name updated with new image: ubuntu:${new_tag}"
 else
